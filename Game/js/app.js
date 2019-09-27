8a1bf1ebc1c519d9031af4777985e28a2558125d
bbsamurai <miohkies@gmail.com>
2019-09-27 20:58:56 +0900

    Added: Initial HTML/CSS
    Changed: Moved the dictionary tests into its on directory.

---
+++ Game/js/app.js
@@ -0,0 +1,11 @@
+/* * * * * * * * * * * * * * * * *
+ *    CMST 495 6380 Group 2      *
+ * * * * * * * * * * * * * * * * *
+ *
+ * Name: app.js
+ * Author: Thuan Bui, Christopher Sankey, Nathan Woodson, Danny Ramirez
+ * Description:
+ *
+ */
+
+window.addEventListener('DOMContentLoaded', init);
